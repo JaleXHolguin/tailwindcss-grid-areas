93f99f107ad02812798c8e7bc00016400e159534
JaleXHolguin <jalex.holguin.dev@gmail.com>
2024-01-02 16:56:36 -0500

    add default utilities

---
+++ src/default-utilities.ts
@@ -0,0 +1,29 @@
+export const defaultGridTemplateAreas = {
+  ".grid-areas-none": {
+    "grid-template-areas": "none",
+  },
+  ".grid-areas-inherit": {
+    "grid-template-areas": "inherit",
+  },
+  ".grid-areas-initial": {
+    "grid-template-areas": "initial",
+  },
+  ".grid-areas-revert": {
+    "grid-template-areas": "revert",
+  },
+  ".grid-areas-revert-layer": {
+    "grid-template-areas": "revert-layer",
+  },
+  ".grid-areas-unset": {
+    "grid-template-areas": "unset",
+  },
+};
+
+export const defaultGridAreas = {
+  ".area-auto": {
+    "grid-area": "auto",
+  },
+  ".area-span": {
+    "grid-area": "span",
+  },
+};
